efa83688b047463593237757b9cc545d119c91c6
Mike McGowan <mmcgowan@dewpoint.com>
2023-10-21 14:25:39 -0400

    Refactor to use 7z

--- Restore-LLProfile.ps1
+++ Restore-LLProfile.ps1
@@ -16,6 +16,6 @@ $ErrorActionPreference = 'SilentlyContinue'
 
 # Create compressed archives of user data to be migrated
 
-Expand-Archive -Path "$Source\$env:USERNAME-UserData.zip" -DestinationPath $Destination
-Expand-Archive -Path "$Source\$env:USERNAME-ChromeData.zip" -DestinationPath "$Destination\AppData\Local\Google\Chrome\User Data"
-Expand-Archive -Path "$Source\$env:USERNAME-EdgeData.zip" -DestinationPath "$Destination\AppData\Local\Microsoft\Edge\User Data"
+& $Source\7za.exe x "$Source\$env:USERNAME-UserData.zip" -o"$Destination" -r
+& $Source\7za.exe x "$Source\$env:USERNAME-ChromeData.zip" -o"$Destination\AppData\Local\Google\Chrome\User Data" -r
+& $Source\7za.exe x "$Source\$env:USERNAME-EdgeData.zip" -o"$Destination\AppData\Local\Microsoft\Edge\User Data" -r
