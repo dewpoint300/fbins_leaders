6e38f391c61302aea89d2e9e0b66af3ee9f7ecf8
Mike McGowan <mmcgowan@dewpoint.com>
2023-10-21 18:58:21 -0400

    Restore username param handling fix

--- Restore-LLProfile.ps1
+++ Restore-LLProfile.ps1
@@ -16,6 +16,6 @@ $ErrorActionPreference = 'SilentlyContinue'
 
 # Create compressed archives of user data to be migrated
 
-& $Source\7za.exe x "$Source\$env:USERNAME-UserData.zip" -o"$Destination" -r
-& $Source\7za.exe x "$Source\$env:USERNAME-ChromeData.zip" -o"$Destination\AppData\Local\Google\Chrome\User Data" -r
-& $Source\7za.exe x "$Source\$env:USERNAME-EdgeData.zip" -o"$Destination\AppData\Local\Microsoft\Edge\User Data" -r
+& $Source\7za.exe x "$Source\$User-UserData.zip" -o"$Destination" -r
+& $Source\7za.exe x "$Source\$User-ChromeData.zip" -o"$Destination\AppData\Local\Google\Chrome\User Data" -r
+& $Source\7za.exe x "$Source\$User-EdgeData.zip" -o"$Destination\AppData\Local\Microsoft\Edge\User Data" -r
