7797fd8977cba9e250e112edfc65a45d69d85cfa
Mike McGowan <mmcgowan@dewpoint.com>
2023-10-21 14:07:50 -0400

    Refactor to use 7zip

--- Capture-LLProfile.ps1
+++ Capture-LLProfile.ps1
@@ -2,20 +2,27 @@
 
  Param(
     
-    [Parameter]
     [String]
     $User = $env:USERNAME,
 
-    [Parameter]
-    [System.IO.Directory]
+    [String]
     $Source = "C:\Users\$User",
 
-    [Parameter]
-    [System.IO.Directory]
+    [String]
     $Destination = "C:\Temp"
 )
 
 $ErrorActionPreference = 'SilentlyContinue'
+
+# Get 7za.exe if it isn't in the destination
+if (!(Test-Path $Destination\7za.exe)){
+$7zURL = "https://github.com/develar/7zip-bin/raw/master/win/x64/7za.exe"
+Invoke-WebRequest -Uri $7zURL -OutFile "$Destination\7za.exe"
+}
+
+# Stop browser processes
+Get-Process msedge* | Stop-Process -Force
+Get-Process chrome* | Stop-Process -Force
 
 # Dump browser cache for efficiency
 # Edge
@@ -25,6 +32,7 @@ Get-ChildItem "$Source\AppData\Local\Microsoft\Edge\User Data\Default\Code Cache
 Get-ChildItem "$Source\AppData\Local\Microsoft\Edge\User Data\Profile*\Service Worker\CacheStorage" -Recurse | Remove-Item -Recurse -Force
 Get-ChildItem "$Source\AppData\Local\Microsoft\Edge\User Data\Profile*\Cache" -Recurse | Remove-Item -Recurse -Force
 Get-ChildItem "$Source\AppData\Local\Microsoft\Edge\User Data\Profile*\Code Cache" -Recurse | Remove-Item -Recurse -Force
+Get-ChildItem "$Source\AppData\Local\Microsoft\Edge\User Data\Crashpad\reports" -Recurse | Remove-Item -Recurse -Force
 # Chrome
 Get-ChildItem "$Source\AppData\Local\Google\Chrome\User Data\Default\Service Worker\CacheStorage" -Recurse | Remove-Item -Recurse -Force
 Get-ChildItem "$Source\AppData\Local\Google\Chrome\User Data\Default\Cache" -Recurse | Remove-Item -Recurse -Force
@@ -32,14 +40,14 @@ Get-ChildItem "$Source\AppData\Local\Google\Chrome\User Data\Default\Code Cache"
 Get-ChildItem "$Source\AppData\Local\Google\Chrome\User Data\Profile*\Service Worker\CacheStorage" -Recurse | Remove-Item -Recurse -Force
 Get-ChildItem "$Source\AppData\Local\Google\Chrome\User Data\Profile*\Cache" -Recurse | Remove-Item -Recurse -Force
 Get-ChildItem "$Source\AppData\Local\Google\Chrome\User Data\Profile*\Code Cache" -Recurse | Remove-Item -Recurse -Force
-
+Get-ChildItem "$Source\AppData\Local\Google\Chrome\User Data\Crashpad\reports" -Recurse | Remove-Item -Recurse -Force
 
 # Create compressed archives of user data to be migrated
-Compress-Archive -Path "$Source\Documents" -Update -DestinationPath $Destination\$User-UserData.zip
-Compress-Archive -Path "$Source\Desktop" -Update -DestinationPath $Destination\$User-UserData.zip
-Compress-Archive -Path "$Source\Pictures" -Update -DestinationPath $Destination\$User-UserData.zip
-Compress-Archive -Path "$Source\AppData\Local\Google\Chrome\User Data" -Update -DestinationPath $Destination\$User-ChromeData.zip
-Compress-Archive -Path "$Source\AppData\Local\Microsoft\Edge\User Data" -Update -DestinationPath $Destination\$User-EdgeData.zip
+& $Destination\7za.exe a $Destination\$User-UserData.zip "$Source\Desktop"
+& $Destination\7za.exe a $Destination\$User-UserData.zip "$Source\Documents"
+& $Destination\7za.exe a $Destination\$User-UserData.zip "$Source\Pictures"
+& $Destination\7za.exe a $Destination\$User-ChromeData.zip "$Source\AppData\Local\Google\Chrome\User Data"
+& $Destination\7za.exe a $Destination\$User-EdgeData.zip "$Source\AppData\Local\Microsoft\Edge\User Data"
 
 # Open explorer to show the files created
 Write-Host "Open explorer to show the files created - please copy these files to the VDI"
